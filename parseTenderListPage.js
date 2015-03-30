1038575cef5af094b6252c9b0802758a372df17a
Archil Bolkvadze <a.bolkvadze@gmail.com>
2015-03-30 19:02:45 +0400

    redefine tender list page parser

--- parseTenderListPage.js
+++ parseTenderListPage.js
@@ -1,24 +1,12 @@
+var idRegex = /^A(\d+)$/;
 module.exports = function(htmlStr) {
   var $ = require('cheerio').load(htmlStr);
-  return $('tbody tr').map(function(i, e) {
-    var id = $(this).attr('id');
-    if(!id || id[0] !== 'A') {
-      return;
-    }
-    id = id.slice(1);
-    var td = $(this).children('td').last().children('p').map(function(){
-      return $(this).text().split('\n').map(function(x){return x.trim();});
-    }).get().reduce(function(acc, textLine) {
-      var segments = textLine.split(':');
-      if(segments.length === 1) {
-        acc.states.push(textLine.trim());
-        acc.states = acc.states.filter(isTruethy);
-      } else {
-        acc[segments[0]] = segments.splice(1).join(':').trim();
-      }
-      return acc;
-    }, {id: id, states: []});
-    return td;
-  }).get().filter(isTruethy);
+  return $('table').filter(function() {
+    var id = $(this).find('tbody tr').first().attr('id');
+    return idRegex.test(id);
+  }).first()
+  .find('tbody tr')
+  .map(function() {
+    return idRegex.exec($(this).attr('id'))[1];
+  }).get();
 };
-function isTruethy(x){return x;}
