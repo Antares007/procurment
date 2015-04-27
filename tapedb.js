e5ad650bb81ab042f96dfe3767211cdb87e58725
Archil Bolkvadze <a.bolkvadze@gmail.com>
2015-04-27 16:38:41 +0400

    can create concurent append streams

--- tapedb.js
+++ tapedb.js
@@ -1,71 +1,72 @@
 var stream = require('stream');
 var assert = require('assert');
 
-module.exports = function(recordsDb){
-
+module.exports = function(db){
+  var tapes = {};
   return {
-    getTape: function(tapeName){
-      var lastRecNo;
-      var keyEncoding = {
-          encode : function (val) { return new Buffer('tape!' + tapeName + '!' + ('00000000000' + val).slice(-11), 'utf8'); },
-          decode : function (val) { return Number(val.toString('utf8').slice(-11)); },
-          buffer : true,
-          type   : 'tape'
-      };
-      var opts = {
-        keyEncoding: keyEncoding,
-        valueEncoding: 'json'
-      };
-
-      return { createReadStream: createReadStream, createAppendStream: createAppendStream };
-
-      function createAppendStream(){
-        var writable = new stream.Writable({
-          objectMode: true,
-          write: function(chunk, encoding, next) {
-            if(typeof lastRecNo === 'undefined'){
-              getLastRecordId(function(err, recNo){
-                assert(typeof lastRecNo === 'undefined');
-                lastRecNo = recNo;
-                writeRecord(chunk, next);
-              });
-            } else {
-              writeRecord(chunk, next);
-            }
-          }
-        });
-        return writable;
-      }
-      function getLastRecordId(cb){
-        var lastKey;
-        var readable = recordsDb.createKeyStream({
-          gte: opts.keyEncoding.encode(0),
-          lte: opts.keyEncoding.encode(99999999999),
-          limit: 1,
-          reverse: true,
-          keyEncoding: opts.keyEncoding
-        }).on('data', function(value){
-          lastKey = value;
-        }).on('end', function(){
-          cb(null, typeof lastKey === 'undefined' ? -1 : lastKey);
-        }).on('error', cb);
-      }
-      function writeRecord(data, cb){
-        recordsDb.put(++lastRecNo, data, opts, cb);
-      }
-
-      function createReadStream(opt){
-        opt = opt || {};
-        var readable = recordsDb.createReadStream({
-          gte: opts.keyEncoding.encode(opt && opt.from ? opt.from : 0),
-          lte: opts.keyEncoding.encode(99999999999),
-          limit: opt.limit,
-          reverse: opt.reverse,
-          keyEncoding: opts.keyEncoding,
-          valueEncoding: opts.valueEncoding
-        });
-        return readable;
+    getTape: function(name){
+      if(tapes[name]){
+        return tapes[name];
+      } else {
+        return tapes[name] = makeTape(db, name);
       }
     }
   };
 };
+
+function makeTape(recordsDb, tapeName){
+  var lastRecNo;
+  var keyEncoding = {
+    encode : function (val) { return new Buffer('tape!' + tapeName + '!' + ('00000000000' + val).slice(-11), 'utf8'); },
+    decode : function (val) { return Number(val.toString('utf8').slice(-11)); },
+    buffer : true,
+    type   : 'tape'
+  };
+  var opts = {
+    keyEncoding: keyEncoding,
+    valueEncoding: 'json'
+  };
+
+  return { createReadStream: createReadStream, createAppendStream: createAppendStream };
+
+  function createAppendStream(){
+    return new stream.Writable({
+      objectMode: true,
+      write: function(chunk, encoding, next) {
+        if(typeof lastRecNo === 'undefined'){
+          getLastRecordId(function(err, recNo){
+            if(err) return next(err);
+            if(typeof lastRecNo === 'undefined'){
+              lastRecNo = recNo;
+            }
+            writeRecord(chunk, next);
+          });
+        } else {
+          writeRecord(chunk, next);
+        }
+      }
+    });
+  }
+  function getLastRecordId(cb){
+    var lastKey = -1;
+    createReadStream({ limit: 1, reverse: true })
+      .on('data', function(data){ lastKey = data.key; })
+      .on('end', function(){ cb(null, lastKey); })
+      .on('error', cb);
+  }
+  function writeRecord(data, cb){
+    recordsDb.put(++lastRecNo, data, opts, cb);
+  }
+
+  function createReadStream(opt){
+    opt = opt || {};
+    return recordsDb.createReadStream({
+      gte: opts.keyEncoding.encode(opt.from || 0),
+      lte: opts.keyEncoding.encode(99999999999),
+      limit: opt.limit,
+      reverse: opt.reverse,
+      keyEncoding: opts.keyEncoding,
+      valueEncoding: opts.valueEncoding
+    });
+  }
+}
