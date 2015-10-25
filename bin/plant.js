1e0344ebbdcd87b6cd58abfa7c7ee91eb9ac0450
Archil Bolkvadze <a.bolkvadze@gmail.com>
2015-10-25 13:02:35 +0400

    change grow plant wtf. need good names

--- bin/plant.js
+++ bin/plant.js
@@ -2,9 +2,13 @@
 'use strict'
 require('babel/register')({ extensions: ['.es6', '.es', '.jsx', '.js'] })
 
+var path = require('path')
+var fs = require('fs')
 var argv = require('yargs').argv
 var mygit = require('../lib/engine/mygit')
 var Commit = require('../lib/commit').Commit
+var scriptPath = path.resolve(process.cwd() + '/' + argv._[0])
+var seedScript = fs.readFileSync(scriptPath, 'utf8')
 
 mygit.getGitDir(function (err, dir) {
   if (err) throw err
@@ -14,7 +18,7 @@ mygit.getGitDir(function (err, dir) {
 
   git.revParse('HEAD')
     .then(function (sha) {
-      return new Commit(sha).grow(new Commit(argv._[0])).getSha(git)
+      return new Commit(sha).plant(seedScript).getSha(git)
     })
     .then(function (sha) {
       batchCat.end()
