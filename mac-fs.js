2b04805e4e3775cda9a7bddfe19eba128d81fc8d
Archil Bolkvadze <a.bolkvadze@gmail.com>
2015-11-11 17:39:46 +0400

    fix fs interface implementation

--- mac-fs.js
+++ mac-fs.js
@@ -1,28 +1,29 @@
 var fs = require('fs')
 var mkdirp = require('mkdirp')
-var debug = require('debug')('mac-fs')
 
 module.exports = {
   rename: fs.rename,
   // - readFile(path) => binary
   //   Must also call callback() with no arguments if the file does not exist.
   readFile: (path, cb) => fs.readFile(path, function (err, buffer) {
-    if (err) {
-      if (err.code === 'ENOENT') return cb()
-      cb(err)
-    }
+    if (err) return err.code === 'ENOENT' ? cb() : cb(err)
     cb(null, buffer)
   }),
   // - readChunk(path, start, end) => binary
   //   Must also call callback() with no arguments if the file does not exist.
   readChunk: (path, start, end, cb) => fs.open(path, 'r', function (err, fd) {
-    var length = end - start
-    var buffer = new Buffer(length)
-    fs.read(fd, buffer, 0, length, start, function (err, bytesRead, buffer) {
+    if (err) return err.code === 'ENOENT' ? cb() : cb(err)
+    fs.fstat(fd, function (err, stats) {
       if (err) return cb(err)
-      if (bytesRead !== length) return cb(new Error('fs.readChunk bytesRead !== length'))
-      cb(null, buffer)
-      fs.close(fd)
+      end = end < 0 ? stats.size + end : end
+      var length = end - start
+      var buffer = new Buffer(length)
+      fs.read(fd, buffer, 0, length, start, function (err, bytesRead, buffer) {
+        if (err) return cb(err)
+        if (bytesRead !== length) return cb(new Error('fs.readChunk bytesRead !== length'))
+        cb(null, buffer)
+        fs.close(fd)
+      })
     })
   }),
   // - writeFile(path, binary) =>
@@ -40,7 +41,7 @@ module.exports = {
   // - readDir(path) => array<paths>
   //   Must also call callback() with no arguments if the file does not exist.
   readDir: (path, cb) => fs.readdir(path, function (err, files) {
-    if (err) return cb()
+    if (err) return err.code === 'ENOENT' ? cb() : cb(err)
     cb(null, files)
   })
 }
