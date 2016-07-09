134d836dd92ef497d1f96b3f4378ba124bb70b18
Archil Bolkvadze <a.bolkvadze@gmail.com>
2016-07-09 19:53:13 +0400

    fake date

--- src/module.js
+++ src/module.js
@@ -3,6 +3,15 @@ const debug = require('debug')('module')
 const runInThisContext = require('vm').runInThisContext
 const path = require('path')
 
+const fakeDate = class Date {
+  getHours () { return 0 }
+  getMinutes () { return 0 }
+  getSeconds () { return 0 }
+  getFullYear () { return 1975 }
+  getMonth () { return 10 }
+  getDate () { return 14 }
+}
+
 class Module {
   constructor (id, filename, parent) {
     this.id = id
@@ -16,13 +25,13 @@ class Module {
   }
 
   _compile (script) {
-    var wrappedCode = `(function (exports, require, module, __filename, __dirname) {${script}})`
+    var wrappedCode = `(function (exports, require, module, __filename, __dirname, Date) {${script}})`
     var compiledWrapper = runInThisContext(wrappedCode, {
       filename: this.id.slice(0, 6) + ':' + this.filename.slice(1),
       lineOffset: 0,
       displayErrors: true
     })
-    const args = [this.exports, this.require, this, this.filename, this.dirname]
+    const args = [this.exports, this.require, this, this.filename, this.dirname, fakeDate]
     compiledWrapper.apply(this.exports, args)
   }
 
